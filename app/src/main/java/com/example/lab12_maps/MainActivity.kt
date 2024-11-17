6663a45417b03e46f27ff081296977dfb14d6ed0
Cheems-code <osmar.ancco@tecsup.edu.pe>
2024-11-17 15:24:25 -0500

    Add: MapScreen y MainActivity

--- app/src/main/java/com/example/lab12_maps/MainActivity.kt
+++ app/src/main/java/com/example/lab12_maps/MainActivity.kt
@@ -18,30 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Lab12_mapsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MapScreen()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab12_mapsTheme {
-        Greeting("Android")
-    }
-}
