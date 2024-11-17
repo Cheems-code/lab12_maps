3120b605b39eec0bc6cb6b1dde855b82df9acaf8
Cheems-code <osmar.ancco@tecsup.edu.pe>
2024-11-17 15:56:29 -0500

    Add: varios iconos en el mapa

--- app/src/main/java/com/example/lab12_maps/MapScreen.kt
+++ app/src/main/java/com/example/lab12_maps/MapScreen.kt
@@ -1,9 +1,9 @@
+package com.example.lab12_maps
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.example.lab12_maps.R
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -18,18 +18,34 @@ fun MapScreen() {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
 
+    // Lista de ubicaciones para los puntos de interés
+    val locations = listOf(
+        LatLng(-16.433415, -71.5442652), // JLByR
+        LatLng(-16.4205151, -71.4945209), // Paucarpata
+        LatLng(-16.3524187, -71.5675994) // Zamacola
+    )
+
     Box(modifier = Modifier.fillMaxSize()) {
         // Añadir GoogleMap al layout
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Arequipa Perú con ícono personalizado
+            // Añadir marcador principal en Arequipa Perú
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
                 icon = BitmapDescriptorFactory.fromResource(R.drawable.monta_a), // Ícono personalizado
                 title = "Arequipa, Perú"
             )
+
+            // Iterar sobre la lista de ubicaciones para añadir marcadores
+            locations.forEach { location ->
+                Marker(
+                    state = rememberMarkerState(position = location),
+                    title = "Ubicación",
+                    snippet = "Punto de interés"
+                )
+            }
         }
     }
 }
